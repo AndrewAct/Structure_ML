9d78036b6bcc8d6e317cc5b66b549d598582e327
Andrew Chen <43218650+AndrewAct@users.noreply.github.com>
2020-01-29 23:49:16 -0500

    First commit of lecture content

---
+++ Coursera_Structure_ML/WEEK2/Lecture.py
@@ -0,0 +1,39 @@
+########## Module 1 ##########
+##       Error Analysis     ##
+
+#  Evaluate multiple ideas in parallel #
+# Ideas for cat detection:
+# - Fix pictures of dogs being recognized as catss
+# - Fix great cats being misrecognized 
+# - Improve performance on blurry images 
+
+# Cleaning up incorrectly labeled data #
+# Correcting incorrect dev/test set examples
+# -Apply same process to your dev and test sets to make sure they continue to come from the same distribution 
+# -Consider examining examples your algorithm got right as wekk as ones it got wrong 
+# -Train and dev/test data may now come from slightly different distributions
+
+# Build your system quickly, the iterate #
+# Speech recognition example
+# -Noisy background 
+# -Accented speech 
+# -Far from micphone
+# -Stuttering
+
+
+# Steps 
+# 1. Set up dev/test set and metric 
+# 2. Build initial system quickly 
+# 3. Use Bias/Variance analysis & Error analysis to prioritize next stops
+
+
+########## Module 2 ##########
+## Mismatched Training and dev/test set ##
+# Cat app example 
+# Data from webapges 
+
+# Data from mobile app
+
+# Speech recognition example 
+# Traning: Purchased data/Smart speaker control/Voice control 
+# Dev/test: Speech activated research mirror
